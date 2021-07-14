c21c338ef50efb616f448ed80831843a815eee5b
Kamil <sikorakamil26@gmail.com>
2021-07-14 20:15:19 +0200

    fix font weights

--- packages/lou_bank_ui/lib/src/typography/lou_typography.dart
+++ packages/lou_bank_ui/lib/src/typography/lou_typography.dart
@@ -14,9 +14,8 @@ abstract class LouTypography {
 }
 
 abstract class LouFontWeight {
-  static const FontWeight thin = FontWeight.w100;
-  static const FontWeight light = FontWeight.w300;
   static const FontWeight regular = FontWeight.w400;
   static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
   static const FontWeight bold = FontWeight.w700;
 }
